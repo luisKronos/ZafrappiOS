52481b8959ea63426e0b6da51856a180fd690a18
Jorge Ivan Estrada Torres <jiet850@icloud.com>
2020-10-09 21:19:34 -0500

    Add messages and setup for labels in VacanciesSavedViewController.

--- Zafrapp/Controllers/vacancies/VacanciesSavedViewController.swift
+++ Zafrapp/Controllers/vacancies/VacanciesSavedViewController.swift
@@ -21,12 +21,17 @@ class VacanciesSavedViewController: ZPMasterViewController {
             static let companiesCellIdentifier = "CompaniesTableViewCell"
             static let heightRow: CGFloat = 100
         }
+        enum String {
+            static let noVacancies = NSLocalizedString("No hay vacantes marcadas.", comment: "")
+            static let noPostulation = NSLocalizedString("No sea ha postulado a ninguna vacante.", comment: "")
+        }
     }
     
     // MARK: - IBOutlets
     
     @IBOutlet private var selectionView: UIView!
     @IBOutlet private var tableView: UITableView!
+    @IBOutlet private var emptyMessageLabel: UILabel!
     
     // MARK: - Properties
     
@@ -34,7 +39,7 @@ class VacanciesSavedViewController: ZPMasterViewController {
     private var postulastionsSaved: [Postulation]?
     private var postulationOption = 1
     private var saved = 2
-    private var typeOfData = 0
+    private var selectedSegmentIndex = 0
     private var genericPostulations: [Postulation]?
     
     // MARK: - Computed Properties
@@ -55,9 +60,10 @@ class VacanciesSavedViewController: ZPMasterViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        configTable()
+        configureLabel()
+        configureTableView()
         executeService(status: 2)
-        addSegmentedControled()
+        configureSegmentedControl()
     }
     
 }
@@ -66,14 +72,30 @@ class VacanciesSavedViewController: ZPMasterViewController {
 
 private extension VacanciesSavedViewController {
     
-    func addSegmentedControled() {
-        let codeSegmented = CustomSegmentedControl(frame: CGRect(x: 0, y: 0, width: self.selectionView.frame.width , height: self.selectionView.frame.height), buttonTitle: ["Mis vacantes marcadas","Mis postulaciones"])
-        codeSegmented.delegate = self
-        codeSegmented.backgroundColor = .clear
-        self.selectionView.addSubview(codeSegmented)
+    func configureLabel() {
+        emptyMessageLabel.numberOfLines = 0
+        emptyMessageLabel.textColor = ZafrappTheme.Color.gray
+        emptyMessageLabel.font = ZafrappTheme.Font.Profile.emptyMessage
+        emptyMessageLabel.text = Constants.String.noVacancies
+        toggleEmptyMessage()
     }
     
-    func configTable() {
+    func configureSegmentedControl() {
+        let codeSegmented = CustomSegmentedControl(frame: .zero, buttonTitle: ["Mis vacantes marcadas", "Mis postulaciones"])
+        codeSegmented.translatesAutoresizingMaskIntoConstraints = false
+        codeSegmented.delegate = self
+        codeSegmented.backgroundColor = .clear
+        selectionView.addSubview(codeSegmented)
+        
+        NSLayoutConstraint.activate([
+            codeSegmented.topAnchor.constraint(equalTo: selectionView.topAnchor),
+            codeSegmented.bottomAnchor.constraint(equalTo: selectionView.bottomAnchor),
+            codeSegmented.leadingAnchor.constraint(equalTo: selectionView.leadingAnchor),
+            codeSegmented.trailingAnchor.constraint(equalTo: selectionView.trailingAnchor)
+        ])
+    }
+    
+    func configureTableView() {
         tableView.separatorStyle = .none
         tableView.delegate = self
         tableView.dataSource = self
@@ -124,7 +146,13 @@ private extension VacanciesSavedViewController {
             } else {
                 self.present(ZPAlertGeneric.oneOption(title: AppConstants.String.errorTitle, message: AppConstants.String.tryAgain, actionTitle: AppConstants.String.accept),animated: true)
             }
+            self.toggleEmptyMessage()
         }
+    }
+    
+    func toggleEmptyMessage() {
+        emptyMessageLabel.isHidden = !(genericPostulations?.isEmpty ?? true)
+        emptyMessageLabel.text = selectedSegmentIndex == postulationOption ? Constants.String.noPostulation : Constants.String.noVacancies
     }
 }
 
@@ -133,9 +161,11 @@ private extension VacanciesSavedViewController {
 extension VacanciesSavedViewController: CustomSegmentedControlDelegate {
     
     func changeToIndex(index: Int) {
-        typeOfData = index
+        selectedSegmentIndex = index
         genericPostulations = index == postulationOption ? postulations : postulastionsSaved
-        self.tableView.reloadData()
+        
+        toggleEmptyMessage()
+        tableView.reloadData()
         
         if index == postulationOption, postulations?.count == nil {
             executeService(status: 1)
@@ -167,10 +197,10 @@ extension VacanciesSavedViewController: UITableViewDelegate, UITableViewDataSour
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let selected = genericPostulations?[indexPath.row] ?? Postulation()
         
-        if typeOfData == 0 {
+        if selectedSegmentIndex == 0 {
             selected.isSaved = true
             selected.isPostulated = false
-        } else if typeOfData == 1{
+        } else if selectedSegmentIndex == 1{
             selected.isPostulated = true
             selected.isSaved = false
         }
